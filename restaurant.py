1512916d1f560f12ba62c9f82949a9886756fc32
Sarisha.tk <sarishakrishna58@gmail.com>
2025-09-26 11:04:24 +0530

    # You're designing a smart billing system for a grocery shop that supports:

---
+++ restaurant.py
@@ -0,0 +1,17 @@
+# You're designing a smart billing system for a grocery shop that supports:
+
+# Showing a menu     >>>>>>>>>> dictionary for product and price
+# ⦁	
+# Taking multiple item orders >>>>>>>>>  if u need you can create a Cart (using a list )
+
+# Generating a bill with, price, total, discount, and final amount
+
+# Supporting coupon codes    >>>>>> if user enter "SAVE10"  10 % offer   | "save20" 20 % offer (only price above 1500)
+
+# Repeated ordering until customer says "no"   >>>> if "yes" show menu again
+
+# Asking for customer's phone number  >>> ask for customers name and phone number (greeting should be added)
+
+# Saving the bill into a text file with customer details >>>> generate a bill in txt file showing (price name ,total)
+
+
