86f3972968fd834c79ea9953ffa1f71baf566d71
Sarisha.tk <sarishakrishna58@gmail.com>
2025-09-26 18:26:27 +0530

    bill generated

--- restaurant.py
+++ restaurant.py
@@ -21,38 +21,63 @@
 # Menu (dictionary of products with prices)
 menu = {
     "Rice": 50,
+
     "Sugar": 40,
+
     "Milk": 30,
+
     "Bread": 25,
+
     "Eggs": 6,
+
     "Oil": 120
 }
 
 print("🛒 Welcome to Smart Grocery Billing System")
 
 # Get customer details
+
 name = input("Enter your name: ")
+
 phone = input("Enter your phone number: ")
+
 print(f"\nHello {name}, let's start shopping!\n")
 
 cart = []   # list to store items
 
 while True:
     print("\nAvailable Items:")
+    
     for item, price in menu.items():
+
         print(f"{item} - ₹{price}")
 
     choice = input("\nEnter the item you want to buy: ")
     
     if choice in menu:
+
         qty = int(input(f"Enter quantity of {choice}: "))
+
         cart.append((choice, qty, menu[choice] * qty))
+
         print(f"✅ {choice} added to cart.")
+
     else:
+
         print("❌ Item not found in menu!")
 
     repeat = input("Do you want to order more? (yes/no): ").lower()
+
     if repeat != "yes":
+
         break
 
     # generate bill
+    
+    total = sum(item[2] for item in cart)
+    
+    for item in cart:
+
+        print(f"{item[0]} x{item[1]} = ₹{item[2]}")
+
+        print(f"Total amount = ₹{total}")
