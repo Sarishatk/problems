6cf9e0b101dcfc2ea4c055a47afcfdec01e61b98
Sarisha.tk <sarishakrishna58@gmail.com>
2025-09-29 13:49:19 +0530

    finished

---
+++ total_sum.py
@@ -0,0 +1,22 @@
+start = 0
+
+total = 0
+
+while(total<50):
+
+     user_input = int(input("Enter a number"))
+
+     total+=user_input
+
+     print(f"the total is {total}")
+
+if total>50:
+          
+          print("exit")
+
+
+
+
+
+
+
