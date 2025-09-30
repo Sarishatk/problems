91772eedd89c91493f01a49df9d5582a6ca3ebe7
Sarisha.tk <sarishakrishna58@gmail.com>
2025-09-30 11:28:59 +0530

    create a variable called copnum and set the value to 50 ask the user to enter a number .while therir guess us is not the same as the comnum value ,tell them if their guess is too low or too high and ask them to have another guess. if they enter the same value as compnum,display

---
+++ compnum.py
@@ -0,0 +1,8 @@
+"""
+create a variable called copnum and set the value to 50 ask the user to enter a number .while therir guess us is not the same as the comnum value
+,tell them if their guess is too low or too high
+and ask them to have another guess.
+if they enter the same value as compnum,display
+"""
+
+
