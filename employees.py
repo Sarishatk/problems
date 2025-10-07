8e5586ea632b226498695d94bd6fcbeb1a1daea9
Sarisha.tk <sarishakrishna58@gmail.com>
2025-10-07 13:38:17 +0530

    attribute set

--- employees.py
+++ employees.py
@@ -1,3 +1,20 @@
 class Employee:
 
-    
+    id:int
+
+    name:str
+
+    department:str
+
+    salary:int
+
+    def set_employee(self,id,name,department,salary):
+
+        self.id = id
+
+        self.name = name
+
+        self.department = department
+
+        self.salary = salary
+
