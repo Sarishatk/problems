06fc9794d6a7158229369fc7eb82c9d362df9748
Sarisha.tk <sarishakrishna58@gmail.com>
2025-10-08 16:54:35 +0530

    class solution created

--- merge_string.py
+++ merge_string.py
@@ -1,2 +1,11 @@
 # given two string a = "abc" b = "sql" merge these two string and form a new string s=[]
 
+class solution:
+
+    def mergetwoarray(self,word1:str,word2:str)->str:
+
+        A,B = len(word1),len(word2)
+
+        a,b = 0,0
+
+        s = []
